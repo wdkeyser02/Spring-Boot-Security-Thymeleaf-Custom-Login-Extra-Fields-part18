600d55385f5a3149c3086515f4e8b6aa35005b88
Willy De Keyser <spring.boot.programming@gmail.com>
2026-03-22 22:32:22 +0100

    Init

---
+++ Spring-Boot-Security-Thymeleaf-Custom-Login-Extra-Fields-part18/src/main/java/willydekeyser/SpringBootSecurityThymeleafCustomLoginExtraFieldsPart18Application.java
@@ -0,0 +1,13 @@
+package willydekeyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityThymeleafCustomLoginExtraFieldsPart18Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityThymeleafCustomLoginExtraFieldsPart18Application.class, args);
+	}
+
+}
